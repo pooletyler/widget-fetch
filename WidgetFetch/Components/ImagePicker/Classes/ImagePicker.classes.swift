aa5b967db5beba60d72cd64063ce03fdbd890ec1
pooletyler <poole.tyler.b@gmail.com>
2021-02-03 12:20:24 -0500

    building again

--- WidgetFetch/Components/ImagePicker/Classes/ImagePicker.classes.swift
+++ WidgetFetch/Components/ImagePicker/Classes/ImagePicker.classes.swift
@@ -5,13 +5,20 @@
 //  Created by Tyler Poole on 2/1/21.
 //
 
-import Foundation
 import UIKit
 
 class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-    let parent: ImagePicker
+    let parent: ImagePickerUIKitBridge
 
-    init(_ parent: ImagePicker) {
+    init(_ parent: ImagePickerUIKitBridge) {
         self.parent = parent
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let uiImage = info[.originalImage] as? UIImage {
+            parent.image = uiImage
+        }
+
+        parent.presentationMode.wrappedValue.dismiss()
     }
 }
